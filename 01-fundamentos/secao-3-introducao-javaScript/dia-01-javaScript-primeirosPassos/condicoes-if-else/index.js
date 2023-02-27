87233295983f02c308846de81d842961dd128c32
Anderson Bruno <anderson.bruno90@gmail.com>
2023-02-27 13:45:22 -0300

    condicoes-if-else

---
+++ 01-fundamentos/secao-3-introducao-javaScript/dia-01-javaScript-primeirosPassos/condicoes-if-else/index.js
@@ -0,0 +1,13 @@
+let score = 99
+
+if (score >=80){
+    console.log('Parabéns,você faz parte do grupo de pessoas aprovadas!')
+}
+
+else if (score >=60 && score <80){
+    console.log('Você está na nossa lista de espera.')
+}
+
+else {
+    console.log('Infelizmente, você reprovou.')
+}
