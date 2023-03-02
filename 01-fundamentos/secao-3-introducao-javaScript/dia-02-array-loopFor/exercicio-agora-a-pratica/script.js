d94407b9f9d8a5517e6ab7a1e07820dc6801334f
Anderson Bruno <anderson.bruno90@gmail.com>
2023-03-02 14:56:56 -0300

    Exercicios-Lógica de programação e algoritmos

--- 01-fundamentos/secao-3-introducao-javaScript/dia-02-array-loopFor/exercicio-agora-a-pratica/script.js
+++ 01-fundamentos/secao-3-introducao-javaScript/dia-02-array-loopFor/exercicio-agora-a-pratica/script.js
@@ -92,3 +92,79 @@ let numbers = [5, 9, , 19, 70, 8, 100, 2, 35, 27]
 
 // for (let index = 0; index <array.length; index +=1)
 // console.log(array[index]/2);
+
+
+// LOGICA DE PROGRAMAÇÃO E ALGORITIMOS
+
+
+// crie um algoritmo que imprima na tela o fatorial de 10
+
+// let fatorial = 1;
+
+// for (let index = 10; index > 0; index -= 1) {
+//   fatorial *= index;
+// }
+
+// console.log(fatorial);
+
+
+
+
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra, como a palavra “banana” para “ananab”. Utilize a string abaixo como exemplo. Depois, troque-a por outras para verificar se seu algoritmo está funcionando corretamente.
+// Copiar
+
+// let word = 'tryber';
+
+// let reverseWord = '';
+
+// for (let index = 0; index < word.length; index += 1) {
+//   reverseWord += word[word.length - 1 - index];
+// }
+
+// console.log(reverseWord);
+
+
+
+
+// Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// let biggestWord = array[0];
+// let smallestWord = array[0];
+
+// for (let index = 1; index < array.length; index += 1) {
+//   if (array[index].length > biggestWord.length) {
+//     biggestWord = array[index];
+//   }
+// }
+
+// for (let index = 1; index < array.length; index += 1) {
+//   if (array[index].length < smallestWord.length) {
+//     smallestWord = array[index];
+//   }
+// }
+
+// console.log(biggestWord);
+// console.log(smallestWord);
+
+
+
+
+// Um número primo é um número inteiro maior que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que imprima no console o maior número primo entre 2 e 50.
+
+// let biggestPrimeNumber = 0;
+
+// for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+//   let isPrime = true;
+//   for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+//     if (currentNumber % currentDivisor === 0) {
+//       isPrime = false;
+//     }
+//   }
+//   if (isPrime) {
+//     biggestPrimeNumber = currentNumber;
+//   }
+// }
+
+// console.log(biggestPrimeNumber);
