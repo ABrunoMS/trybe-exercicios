ec7df6c92e138a763e6070565e2235f1ac55e5a1
Anderson Bruno <anderson.bruno90@gmail.com>
2023-03-14 09:30:36 -0300

    Dia 01 seletores de elementos

---
+++ 01-fundamentos/secao-4-Dom-eventos-webStorage/dia-01-Dom-e-seletores/seletores-elementos/script.js
@@ -0,0 +1,14 @@
+document.getElementById('page-title').innerText = 'O senor dos aneis'
+
+document.getElementById('second-paragraph').style.backgroundColor = 'yellow'
+
+document.getElementById('subtitle').style.backgroundColor = 'orange'
+
+const paragraph = document.getElementsByClassName('paragraphs')
+
+
+
+paragraph[0].style.color = 'red'
+paragraph[0].style.backgroundColor= 'black'
+
+document.getElementsByTagName('h4')[0].style.color = 'blue'
